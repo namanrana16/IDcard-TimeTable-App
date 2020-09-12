4e684924008b397b65e1c80a6df9f7879dd8226b
namanrana <47075510+namanrana16@users.noreply.github.com>
2020-09-12 21:20:37 +0530

    ID-and-Timetable-App

--- app/src/main/java/com/example/appi/MainActivity.kt
+++ app/src/main/java/com/example/appi/MainActivity.kt
@@ -1,30 +1,46 @@
 package com.example.appi
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.viewpager.widget.ViewPager
+import com.example.appi.R.layout.activity_main
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_id.*
+import kotlinx.android.synthetic.main.fragment_timetable.*
 
 class MainActivity : AppCompatActivity() {
+    private val picker = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        xy.setImageURI(uri)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
 
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.idFragment)
 
+        bottomNavigationView.setupWithNavController(navController)
 
-
-
-            xy.setOnClickListener {
-                Toast.makeText(this,"Home button",Toast.LENGTH_SHORT)
-
-                zz.setOnClickListener {
-                    Toast.makeText(this, "Home button", Toast.LENGTH_SHORT)
-                }
-            }
+        val btn = findViewById<Button>(R.id.addidbutton)
+        btn.setOnClickListener {
+            picker.launch("Image/*")
 
         }
+
     }
+
+
+}
